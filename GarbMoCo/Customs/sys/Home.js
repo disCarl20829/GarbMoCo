e5e1f634e018f455dbe8ddcded58cec4beaa085e
disCarl20829 <carl.schoolacc208@gmail.com>
2025-08-19 18:31:35 +0800

    Add files via upload

---
+++ GarbMoCo/Customs/sys/Home.js
@@ -0,0 +1,30 @@
+function showPass() {
+  var password = document.getElementById("password");
+
+  if (password.type == "password") {
+    password.type = "text"
+  } else {
+    password.type = "password"
+  }
+}
+
+function buttonLog() {
+ const username = document.getElementById('username');
+ const inputUsername = username.value
+
+ const password = document.getElementById('password');
+ const inputPassword = password.value
+
+ const getPassword = "abc123"
+ const getUsername = "gwapoko123"
+
+ if (inputPassword == getPassword && inputUsername == getUsername) {
+  alert("Logging you in\nWelcome " + getUsername + "!")
+  window.open("homepage.html", "_self")
+  window.close
+ } else if (inputUsername !== getUsername) {
+  alert("Invalid Username")
+ } else {
+  alert("Invalid Password. Please try again")
+ }
+}
